164c92eb6d53b9873a2856cd7a74a77767101be7
David Zhang <dzz0615@gmail.com>
2023-06-27 23:19:57 -0700

    bumped up default timeout

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // completion request
 export const RateLimitRetryIntervalMs = 30_000;
 export const CompletionDefaultRetries = 3;
-export const CompletionDefaultTimeout = 60_000;
+export const CompletionDefaultTimeout = 300_000;
 export const MinimumResponseTokens = 200;
