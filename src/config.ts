cdb53552fdc0eaceb5fbee9b8a25d37072cec2c7
David Zhang <dzz0615@gmail.com>
2023-07-11 12:16:41 -0700

    updated default claude model

--- src/config.ts
+++ src/config.ts
@@ -6,5 +6,5 @@ export const MinimumResponseTokens = 200;
 export const MaximumResponseTokens = 8_000;
 
 export const DefaultOpenAIModel = 'gpt-3.5-turbo';
-export const DefaultAnthropicModel = 'claude-instant-1-100k';
+export const DefaultAnthropicModel = 'claude-instant-1';
 export const DefaultAzureVersion = '2023-06-01-preview';
