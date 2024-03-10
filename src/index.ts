f86296c397d0c77cbc607b7db3cc0858f1c3441c
David Zhang <dzz0615@gmail.com>
2024-03-09 23:42:55 -0800

    now exporting groq

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './models/errors';
 export * from './models/openai';
 export * from './models/anthropic';
 export * from './models/anthropic-bedrock';
+export * from './models/groq';
 export * from './models/interface';
 export * from './types';
